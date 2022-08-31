6cbaaa91c70be96a20c8d18eb5f5e89b6b878e49
RealLinen <94056347+RealLinen@users.noreply.github.com>
2022-08-30 21:35:59 -0400

    Update AimbotModuleV2.lua

--- Community/AimbotModuleV2.lua
+++ Community/AimbotModuleV2.lua
@@ -6,20 +6,30 @@ AimbotModule.SetPart = function(p: Instance)
     AimbotModule.__Part = p
 end
 AimbotModule.LockToPart = function(part--[[ the part you want to lock on/Instance ]], seconds--[[ seconds it should lock on the player for ]], cam--[[If you want the mouse to move onto the target]])
+return ({pcall(function()
     repeat task.wait() until not AimbotModule.Locking
     part = typeof(part)=="Instance" and part or typeof(AimbotModule.__Part)=="Instance" and AimbotModule.__Part or nil;if not part then return; end
-    seconds = typeof(seconds)=="number" and seconds or 1
+    seconds = typeof(seconds)=="number" and seconds or nil
 
     AimbotModule.Locking = true;
     local finished = false;task.delay(seconds+.1, function() finished = true end)
-    while task.wait() and not finished do
+    if seconds then
+        while task.wait() and not finished do
+            local screenVec, onScreen = AimbotModule.Camera:WorldToViewportPoint(part.Position)
+            AimbotModule.Camera.CFrame = CFrame.new(AimbotModule.Camera.CFrame.Position, (part.CFrame and part.CFrame.Position or part.Position))
+            if cam then
+                AimbotModule.mousemove(screenVec.X, screenVec.Y) 
+            end
+        end
+    else
         local screenVec, onScreen = AimbotModule.Camera:WorldToViewportPoint(part.Position)
         AimbotModule.Camera.CFrame = CFrame.new(AimbotModule.Camera.CFrame.Position, (part.CFrame and part.CFrame.Position or part.Position))
         if cam then
             AimbotModule.mousemove(screenVec.X, screenVec.Y) 
         end
     end
-    AimbotModule.Locking = false;
+    AimbotModule.Locking = false;    
+end)})[2]
 end
 --===================--
 --[[
