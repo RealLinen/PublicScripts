c875cb42a11f901961d58883073f772aed619e6f
RealLinen <94056347+RealLinen@users.noreply.github.com>
2022-11-06 16:32:57 -0500

    Update LoopModule.lua

--- Community/LoopModule.lua
+++ Community/LoopModule.lua
@@ -1,13 +1,15 @@
 local RunService = game:GetService("RunService")
 local isver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RealLinen/PublicScripts/main/Community/isver.lua"))();
 local LoopModule = { ["isver"] = isver }
+local ThisData = { ["Running"] = {} }
 local Calling = 1
 local Heartbeat = RunService.Heartbeat:Connect(function(deltaTime, ...)
     if not isver() then return; end
     if type(Calling)~="number" then return; end
     local func = LoopModule[Calling]
-    if type(func)~="function" then Calling = 1;return true end;Calling = Calling + 1
-    local suc,err = pcall(func, deltaTime, ...);if not suc then return warn(string.format("LoopModule Function #%s Errored:\n%s", Calling, err)); end
+    local oldCalling = Calling
+    if type(func)~="function" then Calling = 1;return true end;if ThisData["Running"][Calling] then Calling = Calling + 1;return;end;Calling = Calling + 1
+    ThisData["Running"][oldCalling]=true;local suc,err = pcall(func, deltaTime, ...);if not suc then warn(string.format("LoopModule Function #%s Errored:\n%s", oldCalling, err)); end;ThisData["Running"][oldCalling] = false
 end)
 setmetatable(LoopModule, {
     __call = function(tb, func)
@@ -18,14 +20,12 @@ setmetatable(LoopModule, {
         end
     end
 })
-task.spawn(function()
-    if not isver() and Heartbeat then
-        pcall(function()
-            Heartbeat:Disconnect()
-            Heartbeat:Unconnect()
-            Heartbeat:Remove()
-        end)
-    end;return true
-end)
 -----------------------
-return LoopModule
+return LoopModule, (function()
+    repeat task.wait() until Heartbeat and not isver()
+    pcall(function()
+        Heartbeat:Disconnect()
+        Heartbeat:Unconnect()
+        Heartbeat:Remove()
+    end)
+end)()
