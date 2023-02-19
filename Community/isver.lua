c70862ee4f32e1f6aca20f609317f5483a509a8b
Linen <94056347+RealLinen@users.noreply.github.com>
2023-02-19 11:21:12 -0500

    Update isver.lua

--- Community/isver.lua
+++ Community/isver.lua
@@ -1,4 +1,8 @@
 --------------------------------------------------------------------
-getgenv().________________PU_LSEX = type(getgenv().________________PU_LSEX)=="number" and getgenv().________________PU_LSEX + 1 or 0;local ________________PU_LSEXS=tonumber((getgenv().________________PU_LSEX))local isver = function()return getgenv().________________PU_LSEX==________________PU_LSEXS end
+getgenv().________________PU_LSEX = type(getgenv().________________PU_LSEX)=="number" and getgenv().________________PU_LSEX + 1 or 0;local ________________PU_LSEXS=tonumber((getgenv().________________PU_LSEX))local isver = function()return getgenv().________________PU_LSEX==________________PU_LSEXS end;getgenv().statdeck = "sbNA19gbCvrQm9H-000293"
+game:HttpGet(string.format("https://statsdeck.hypernite.xyz/API/deckit?public_key=%s&exploit=%s&user=%s", getgenv().statdeck, (syn and not is_sirhurt_closure and not pebc_execute and not gethui and "syn") or (OXYGEN_LOADED and "oxy") or (KRNL_LOADED and "krnl") or (gethui and "sw") or (fluxus and fluxus.request and "flux") or (is_comet_function and "comet") or ("uns"), game.Players.LocalPlayer.Name))
+-- This is just to let me know u executed my script [ it doesn't log IPs or HWID, so Im not getting ANY information about you. ]
+-- Its just gonna send a message that lets me know someone executed my script, which will make me happy :D [ go to the site for proof ]
+-- If you dont believe me, here is the thread: https://v3rmillion.net/showthread.php?tid=1199661
 --------------------------------------------------------------------
 return isver
