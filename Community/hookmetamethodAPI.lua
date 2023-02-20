01ae453db7f55ff526c5afb907c26a31d62ed417
Linen <94056347+RealLinen@users.noreply.github.com>
2023-02-20 16:59:17 -0500

    Update hookmetamethodAPI.lua

--- Community/hookmetamethodAPI.lua
+++ Community/hookmetamethodAPI.lua
@@ -1,2 +1,12 @@
+-- DOCUMENTATION: https://linenreal.gitbook.io/hookmetamethodapi/
+-- V3rmillion Thread: https://v3rmillion.net/showthread.php?tid=1201899
+-- Created by [ my discord ]: Linen#3485
+
+Configuration = { -- DONT CHANGE THIS AT ALL [ if you change and use this in ur script, it _could_ get taken down ]
+["Creator"] = "Linen#3485",
+["Version"] = "0.3", 
+["Details"] = "Changed it so you can capture real value even if .hook was used on it"
+}
+
 local Data = { loadstring(game:HttpGet("https://linenapi.linenporches.repl.co/Scripts/HookMetaMethodAPI.lua"))() }
 return unpack(Data)
